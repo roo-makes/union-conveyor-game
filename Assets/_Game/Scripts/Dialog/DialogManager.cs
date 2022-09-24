7c47647146e48beaf655870f275941b5a2b7809d
Andrew Metcalf <met5678@gmail.com>
2022-09-24 00:07:13 -0400

    Adding flat zones, working on sanitizer charge

--- Assets/_Game/Scripts/Dialog/DialogManager.cs
+++ Assets/_Game/Scripts/Dialog/DialogManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Ink.Runtime;
 using TMPro;
 using UnityEngine;
@@ -10,11 +11,30 @@ public class DialogManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _textBox;
     private Story _inkStory;
 
+    public enum DialogType
+    {
+        Standard,
+        StageDirection,
+        Choices
+    }
+
+    public bool CanContinue => _inkStory.canContinue;
+    public bool IsChoice => !CanContinue && _inkStory.currentChoices.Count > 0;
+    public string DialogText => _inkStory.currentText;
+    public 
+
     void Awake()
     {
         _inkStory = new Story(_inkAsset.text);
     }
+
+    public void Advance()
+    {
+        if (CanContinue) _inkStory.Continue();
+    }
     
+    
+
     public string GetNextStoryText()
     {
         return _inkStory.Continue();
